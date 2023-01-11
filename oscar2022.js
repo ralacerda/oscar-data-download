4c3cf2b4fef9c7f88c39bf1f0d48e127f5e495c2
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-01-10 22:10:01 -0300

    fix: include awards from 2022

---
+++ oscar2022.js
@@ -0,0 +1,254 @@
+export default [
+  {
+    shortName: "filme",
+    winner: "CODA",
+    nominees: [
+      { title: "Belfast" },
+      { title: "CODA" },
+      { title: "Don't Look Up" },
+      { title: "Drive My Car" },
+      { title: "Dune" },
+      { title: "King Richard" },
+      { title: "Licorice Pizza" },
+      { title: "Nightmare Alley" },
+      { title: "The Power of the Dog" },
+      { title: "West Side Story" },
+    ],
+  },
+  {
+    //   ACTRESS IN A SUPPORTING ROLE
+    shortName: "atriz-coadjuvante",
+    winner: "Ariana DeBose",
+    nominees: [
+      { nominee: "Jessie Buckley", title: "The Lost Daughter" },
+      { nominee: "Ariana DeBose", title: "West Side Story" },
+      { nominee: "Judi Dench", title: "Belfast" },
+      { nominee: "Kirsten Dunst", title: "The Power of the Dog" },
+      { nominee: "Aunjanue Ellis", title: "King Richard" },
+    ],
+  },
+
+  // {
+  //   ACTOR IN A SUPPORTING ROLE
+  {
+    shortName: "ator-coadjuvante",
+    winner: "Troy Kotsur",
+    nominees: [
+      { nominee: "Ciaran Hinds", title: "Belfast" },
+      { nominee: "Troy Kotsur", title: "CODA" },
+      { nominee: "Jesse Plemons", title: "The Power of the Dog" },
+      { nominee: "J.K. Simmons", title: "Being the Ricardos" },
+      { nominee: "Kodi Smit-McPhee", title: "The Power of the Dog" },
+    ],
+    //   {INTERNATIONAL FEATURE FILM
+  },
+  {
+    shortName: "estrangeiro",
+    winner: "Drive My Car",
+    nominees: [
+      { title: "Drive My Car" },
+      { title: "Flee" },
+      { title: "The Hand of God" },
+      { title: "Lunana: A Yak in the Classroom" },
+      { title: "The Worst Person in the World" },
+    ],
+
+    //   {DOCUMENTARY FEATURE
+  },
+  {
+    shortName: "documentario",
+    winner:
+      "Summer of Soul (...Or, When the Revolution Could Not Be Televised)",
+    nominees: [
+      { title: "Ascension" },
+      { title: "Attica" },
+      { title: "Flee" },
+      {
+        title:
+          "Summer of Soul (...Or, When the Revolution Could Not Be Televised)",
+      },
+      { title: "Writing with Fire" },
+    ],
+    //   {ORIGINAL SONG
+  },
+  {
+    shortName: "cancao-original",
+    winner: "No Time to Die",
+    nominees: [
+      { title: "King Richard", song: "Be Alive" },
+      { title: "Encanto", song: "Dos Oruguitas" },
+      { title: "Belfast", song: "Down To Joy" },
+      { title: "No Time to Die", song: "No Time to Die" },
+      { title: "Four Good Days", song: "Somehow You Do" },
+    ],
+    //   {ANIMATED FEATURE FILM
+  },
+  {
+    shortName: "animacao",
+    winner: "Encanto",
+    nominees: [
+      { title: "Encanto" },
+      { title: "Flee" },
+      { title: "Luca" },
+      { title: "The Mitchells vs the Machines" },
+      { title: "Raya and the Last Dragon" },
+    ],
+    //   {ADAPTED SCREENPLAY
+  },
+  {
+    shortName: "roteiro-adaptado",
+    winner: "CODA",
+    nominees: [
+      { title: "CODA" },
+      { title: "Drive My Car" },
+      { title: "Dune" },
+      { title: "The Lost Daughter" },
+      { title: "The Power of the Dog" },
+    ],
+    //   {ORIGINAL SCREENPLAY
+  },
+  {
+    shortName: "roteiro-original",
+    winner: "Belfast",
+    nominees: [
+      { title: "Belfast" },
+      { title: "Don't Look Up" },
+      { title: "King Richard" },
+      { title: "Licorice Pizza" },
+      { title: "The Worst Person in the World" },
+    ],
+    //   {ACTOR IN A LEADING ROLE
+  },
+  {
+    shortName: "ator",
+    winner: "Will Smith",
+    nominees: [
+      { nominee: "Javier Bardem", title: "Being the Ricardos" },
+      { nominee: "Benedict Cumberbatch", title: "The Power of the Dog" },
+      { nominee: "Andrew Garfield", title: "tick, tick... BOOM!" },
+      { nominee: "Will Smith", title: "King Richard" },
+      { nominee: "Denzel Washington", title: "The Tragedy of Macbeth" },
+    ],
+    //   {ACTRESS IN A LEADING ROLE
+  },
+  {
+    shortName: "atriz",
+    winner: "Jessica Chastain",
+    nominees: [
+      { nominee: "Jessica Chastain", title: "The Eyes of Tammy Faye" },
+      { nominee: "Olivia Colman", title: "The Lost Daughter" },
+      { nominee: "Penelope Cruz", title: "Parallel Mothers" },
+      { nominee: "Nicole Kidman", title: "Being the Ricardos" },
+      { nominee: "Kristen Stewart", title: "Spencer" },
+    ],
+    //   {DIRECTOR
+  },
+  {
+    shortName: "direcao",
+    winner: "Jane Campion",
+    nominees: [
+      { nominee: "Kenneth Branagh", title: "Belfast" },
+      { nominee: "Ryusuke Hamaguchi", title: "Drive My Car" },
+      { nominee: "Paul Thomas Anderson", title: "Licorice Pizza" },
+      { nominee: "Jane Campion", title: "The Power of the Dog" },
+      { nominee: "Steven Spielberg", title: "West Side Story" },
+    ],
+    //   {PRODUCTION DESIGN
+  },
+  {
+    shortName: "design",
+    winner: "Dune",
+    nominees: [
+      { title: "Dune" },
+      { title: "Nightmare Alley" },
+      { title: "The Power of the Dog" },
+      { title: "The Tragedy of Macbeth" },
+      { title: "West Side Story" },
+    ],
+    //   {CINEMATOGRAPHY
+  },
+  {
+    shortName: "fotografia",
+    winner: "Dune",
+    nominees: [
+      { title: "Dune" },
+      { title: "Nightmare Alley" },
+      { title: "The Power of the Dog" },
+      { title: "The Tragedy of Macbeth" },
+      { title: "West Side Story" },
+    ],
+    // {  COSTUME DESIGN
+  },
+  {
+    shortName: "figurino",
+    winner: "Cruella",
+    nominees: [
+      { title: "Cruella" },
+      { title: "Cyrano" },
+      { title: "Dune" },
+      { title: "Nightmare Alley" },
+      { title: "West Side Story" },
+    ],
+    //   {ACHIEVEMENT IN SOUND
+  },
+  {
+    shortName: "som",
+    winner: "Dune",
+    nominees: [
+      { title: "Belfast" },
+      { title: "Dune" },
+      { title: "No Time to Die" },
+      { title: "The Power of the Dog" },
+      { title: "West Side Story" },
+    ],
+
+    //   {ORIGINAL SCORE
+  },
+  {
+    shortName: "trilha-sonora",
+    winner: "Dune",
+    nominees: [
+      { title: "Don't Look Up" },
+      { title: "Dune" },
+      { title: "Encanto" },
+      { title: "Parallel Mothers" },
+      { title: "The Power of the Dog" },
+    ],
+    //   {VISUAL EFFECTS
+  },
+  {
+    shortName: "efeitos-especiais",
+    winner: "Dune",
+    nominees: [
+      { title: "Dune" },
+      { title: "Free Guy" },
+      { title: "No Time to Die" },
+      { title: "Shang-Chi and the Legend of the Ten Rings" },
+      { title: "Spider-Man: No Way Home" },
+    ],
+    //   FILM EDITING
+  },
+  {
+    shortName: "montagem",
+    winner: "Dune",
+    nominees: [
+      { title: "Don't Look Up" },
+      { title: "Dune" },
+      { title: "King Richard" },
+      { title: "The Power of the Dog" },
+      { title: "tick, tick... BOOM!" },
+    ],
+    // {  MAKEUP AND HAIRSTYLING
+  },
+  {
+    shortName: "maquiagem-e-penteado",
+    winner: "The Eyes of Tammy Faye",
+    nominees: [
+      { title: "Coming 2 America" },
+      { title: "Cruella" },
+      { title: "Dune" },
+      { title: "The Eyes of Tammy Faye" },
+      { title: "House of Gucci" },
+    ],
+  },
+];
